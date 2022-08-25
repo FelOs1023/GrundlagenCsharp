238e5aa3d654468b7d4c1f648d153c16972d85ed
FelOs1023 <oskar@salzburg.link>
2022-08-25 08:35:08 +0200

    Aufgabe 1 bearbeitet

--- 1_Uebungen_Verzweigungen/Program.cs
+++ 1_Uebungen_Verzweigungen/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 9, z = 6; double y = 8;
 
-Console.WriteLine("Irgendwas");
+Console.WriteLine(x-- == y);                        //Ausgabe False
+
+Console.WriteLine(x==(int)y);                       //Ausgabe False
+
+Console.WriteLine("{0:F1}",0.123);                  //Ausgabe 0,1
+
+Console.WriteLine(z/y);                             //Ausgabe 0,75
+
+Console.WriteLine(x-=10);                           //Ausgabe -1
+
+Console.WriteLine(x--== y & true);                  //Ausgabe False
+
+Console.WriteLine(!(z == 6) | false);               //Ausgabe False
+
+Console.WriteLine((x*=-2) == z );                   //Ausgabe False
+
+
 

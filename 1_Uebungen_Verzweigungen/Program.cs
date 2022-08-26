49bdb42b70bc6b66b80437e01effe6be90b020b8
FelOs1023 <oskar@salzburg.link>
2022-08-26 13:24:30 +0200

    Eingeabe einer Temperatur und Abfrage welche art von Einheit benutzt wird für die eingegebene Zahl.
    Umrechnung von Celsius zu Kelvin und Fahrenheit per If.

--- 1_Uebungen_Verzweigungen/Program.cs
+++ 1_Uebungen_Verzweigungen/Program.cs
@@ -1,4 +1,6 @@
-﻿int x = 9, z = 6; double y = 8;
+﻿//Aufgabe 1
+/*
+int x = 9, z = 6; double y = 8;
 
 Console.WriteLine(x-- == y);                        //Ausgabe False
 
@@ -15,6 +17,51 @@ Console.WriteLine(x--== y & true);                  //Ausgabe False
 Console.WriteLine(!(z == 6) | false);               //Ausgabe False
 
 Console.WriteLine((x*=-2) == z );                   //Ausgabe False
+*/
+
+//Aufgabe 2
+
+Console.Write("Bitte gib eine Temperatur in Kelvin oder Celsius an: ");
+double Temp = float.Parse(Console.ReadLine());
+Console.WriteLine("In welcher Einheit ist ihre Temperatur?" +
+    
+    "\n(1) Celsius\n" + 
+    "(2) Kelvin");
+Console.Write("Ihre Auswahl: ");
+double Temperatur = double.Parse(Console.ReadLine());
+
+if (Temperatur == 2)
+{
+    Temp = (Temp - 32) * 5 / 9;
+    Console.WriteLine(Temp + "°C");
+}
+
+if (Temperatur == 1)
+{
+    Console.WriteLine("In welche Einheit möchten sie umrechnen?" +
+    "\n(1)Celsius zu Kelvin" +
+    "\n(2)Celsius zu Fahrenheit");
+    double Celsius_Rech = double.Parse(Console.ReadLine());
+
+    if (Celsius_Rech == 1)
+    {
+        Temp = Temp + 273.15;
+        Console.WriteLine(Temp);
+    }
+    else if (Celsius_Rech == 2)
+    {
+        Temp = (Temp * 9 / 5) + 32;
+        Console.WriteLine(Temp);
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
